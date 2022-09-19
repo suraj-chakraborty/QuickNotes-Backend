80affb6b21a0eb585fe70cb3f6b75bf15f8ee229
suraj-chakraborty <surajchakraborty211@gmail.com>
2022-09-19 12:28:41 +0000

    Quick notes BACKEND 5.3

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const mongoose = require("mongoose");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
