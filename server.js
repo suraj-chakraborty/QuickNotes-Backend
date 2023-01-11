4bb6ab7256abee26b574e81f8a2435fa3468998d
suraj-chakraborty <surajchakraborty211@gmail.com>
2023-01-11 22:32:56 +0530

    fix error

--- server.js
+++ server.js
@@ -4,38 +4,29 @@ const http = require("http");
 const mongoose = require("mongoose");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server, {
-  cors: {
-    origin: [
-      "https://quick-notes-frontend.vercel.app",
-      "https://cute-pear-newt-tux.cyclic.app/",
-    ],
-    method: ["GET", "POST"],
-    transports: ["polling"],
-    credentials: true,
-  },
-  allowEI03: true,
-  loadBalancingMethod: "least-connection",
-});
 const Document = require("./Document");
 
+const PORT = process.env.PORT || 8000;
+
 mongoose.connect(
-  "mongodb+srv://Quicknotes:Quicknotes@quicknotes.rfuqvid.mongodb.net/quick-notes?retryWrites=true&w=majority"
+  process.env.MONGODB_URL ||
+    "mongodb+srv://Quicknotes:Quicknotes@quicknotes.rfuqvid.mongodb.net/quick-notes?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
 );
 
-// const io = require("socket.io")(process.env.PORT || 8080, {
-//   cors: {
-//     origin: "https://quick-notes-frontend.vercel.app",
-//     method: ["GET", "POST"],
-//     credentials: true,
-//   },
-// });
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000" || process.env.CLIENT_URL,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 const defaultValue = "";
 
 io.on("connection", (socket) => {
   socket.on("get-document", async (documentId) => {
-    const document = await Focd(documentId);
+    const document = await getOrCreateDocument(documentId);
     socket.join(documentId);
     socket.emit("load-document", document.data);
 
@@ -49,19 +40,21 @@ io.on("connection", (socket) => {
   });
 });
 
-io.listen(process.env.PORT, () => {
-  console.log("listening on 8080");
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
 
 io.on("connect_error", (err) => {
   console.log(`connect_error due to ${err.message}`);
 });
 
-// find if the documentid exist else create a new document
-async function Focd(id) {
-  if (id == null) return;
+async function getOrCreateDocument(id) {
+  if (!id) return;
 
-  const document = await Document.findById(id);
+  let document = await Document.findById(id);
   if (document) return document;
-  return await Document.create({ _id: id, data: defaultValue });
+
+  document = new Document({ _id: id, data: defaultValue });
+  await document.save();
+  return document;
 }
